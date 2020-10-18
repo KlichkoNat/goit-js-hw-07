632e46497aadbe2616f9e089dda0342924e80b6b
Natali <klichko.nata2708@gmail.com>
2020-10-18 18:36:07 +0300

    1-3задачи

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+const getText = ingredients.map(ingredient => {
+    const ingredientsEl = document.createElement("li");
+    ingredientsEl.textContent = ingredient;
+
+    return ingredientsEl;
+})
+document.querySelector(`#ingredients`).append(...getText);
+
+ 
+
+
+
+
+/*
+Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, 
+после чего вставит все li за одну операцию в список ul.ingredients. 
+Для создания DOM-узлов используй document.createElement().
+*/
